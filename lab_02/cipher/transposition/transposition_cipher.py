5fa7eaafdde1a78d848c885333911729ac8e38fe
dungle611 <letrungdung.9a10@gmail.com>
2025-03-15 11:18:30 +0700

    Update transposition_cipher.py

--- lab_02/cipher/transposition/transposition_cipher.py
+++ lab_02/cipher/transposition/transposition_cipher.py
@@ -1,23 +1,31 @@
+import math
+
 class TranspositionCipher:
     def __init__(self):
         pass
 
     def encrypt(self, text, key):
-        encrypted_text = ''
+        encrypted_text = [''] * key
         for col in range(key):
             pointer = col
             while pointer < len(text):
-                encrypted_text += text[pointer]
+                encrypted_text[col] += text[pointer]
                 pointer += key
-        return encrypted_text
+        return ''.join(encrypted_text)
 
     def decrypt(self, text, key):
-        decrypted_text = [''] * key
-        row, col = 0, 0
+        num_cols = math.ceil(len(text) / key) 
+        num_rows = key 
+        num_shaded_boxes = (num_cols * num_rows) - len(text)
+
+        decrypted_text = [''] * num_cols 
+        col, row = 0, 0
+
         for symbol in text:
             decrypted_text[col] += symbol
             col += 1
-            if col == key or (col == key - 1 and row >= len(text) % key):
+            if col == num_cols or (col == num_cols - 1 and row >= num_rows - num_shaded_boxes):
                 col = 0
                 row += 1
+        
         return ''.join(decrypted_text)
